b371762f875fa48dd2dd2f1832d1d00e3ba92ccc
chenzhijie2020 <74295601+chenzhijie2020@users.noreply.github.com>
2020-11-19 19:48:11 +0800

    Create 111910.性别.c

---
+++ 111910.性别.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+	int a;
+	printf("用户输入");
+	scanf("%d",&a);
+	if(a==1){
+		printf("输出男\n");
+	}
+	else if(a==0){
+		printf("输出女\n");
+	}
+	else if(a>1||a<0){
+		printf("输入有误，请重新输入。");
+	}
+
+
+}
