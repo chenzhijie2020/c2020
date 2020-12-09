5d3daf6d6948d75ad33902f8ff43ae3ba41af5b8
chenzhijie2020 <74295601+chenzhijie2020@users.noreply.github.com>
2020-12-09 17:41:12 +0800

    Create 120906.吃货给安排.app.c

---
+++ 120906.吃货给安排.app.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+	char a1[]="请问现在是什么节";
+	printf("%s\n",a1);
+	scanf("%s",&a1);	
+	if(!strcmp(a1,"春节")){
+	printf("给我安排饺子\n");
+	}
+	else if(!strcmp(a1,"端午节")){
+	printf("给我安排粽子\n");
+	}
+	else if(!strcmp(a1,"中秋节")){
+	printf("给我安排月饼\n");
+	}
+	else if(!strcmp(a1,"元宵节")){
+	printf("给我安排元宵\n");
+	}
+	else{
+	printf("请重新输入");
+	}
+}
